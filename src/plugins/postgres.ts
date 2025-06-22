d920f6fc914e90a61aa0355f3d4b3fc78101bf80
Ibrahim Rouis <ibrahimrouisdev@proton.me>
2025-06-22 14:42:14 +0100

    Add database connection wrapper

--- src/plugins/postgres.ts
+++ src/plugins/postgres.ts
@@ -9,11 +9,22 @@ export default fp<PostgresPluginOptions>(async (fastify, opts) => {
     connectionString: process.env.DATABASE_URL,
   });
 
+  fastify.decorate(
+    'db_connection_wrapper',
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async function (callback: (client: any) => Promise<void>) {
+      const client = await this.pg.connect();
+      try {
+        await callback(client);
+      } finally {
+        client.release();
+      }
+    },
+  );
+
   // Check if database connection is working
   fastify.get('/health/db', async (request, reply) => {
-    const client = await fastify.pg.connect();
-
-    try {
+    await fastify.db_connection_wrapper(async (client) => {
       const { rows } = await client.query('SELECT 1 AS ok');
       if (rows[0].ok === 1) {
         reply.send({ message: 'Database is working' });
@@ -24,8 +35,16 @@ export default fp<PostgresPluginOptions>(async (fastify, opts) => {
           statusCode: 500,
         });
       }
-    } finally {
-      client.release();
-    }
+    });
   });
 });
+
+// Specify added properties for Typescript
+declare module 'fastify' {
+  export interface FastifyInstance {
+    db_connection_wrapper(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      callback: (client: any) => Promise<void>,
+    ): Promise<void>;
+  }
+}
